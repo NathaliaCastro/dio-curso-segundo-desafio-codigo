d7fbbdeb84e925133c29e6396e8ca3480e03cea8
NathaliaCastro <nathalia_gmc@hotmail.com>
2024-04-27 17:03:45 -0300

    Meu segundo desafio de codigo com a DIO

---
+++ SegundoDesafio/CalculandoDano.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class CalculandoDano
+{
+    static int CalcularDano(int ataque, int defesa)
+    {
+        //TODO: Implemente a função calcular dano        
+        int dano = ataque - defesa;
+          //Dano recebe o resultado de ATAQUE - DEFESA
+
+          //Validando o valor do dano
+        if (dano < 0) 
+        {
+            // Se DANO for menor que ZERO, retornar 0
+            return 0;
+        }
+        else 
+        {
+            //Se DANO for MAIOR que ZERO, retornar o VALOR DANO
+            return dano;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+			//Fora da plataforma: Console.WriteLine("Informe o valor do ataque: ");
+        int ataque = int.Parse(Console.ReadLine());
+
+			//Fora da plataforma: Console.WriteLine("Informe o valor de defesa: ");
+        int defesa = int.Parse(Console.ReadLine());
+
+        int danoCausado = CalcularDano(ataque, defesa);
+        Console.WriteLine("O dano causado pelo ataque foi: " + danoCausado);
+    }
+}
---
+++ SegundoDesafio/GuardiaoDosAtributos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class GuardiaoDosAtributos
+{
+    static bool VerificarAtributo(string atributo, int valorMinimo, int valorMaximo, int valorAtributo)
+    {
+        //TODO: Implemente a função VerificarAtributo
+        
+          //Validando valor do atributo (ambas as condições tem que ser verdadeira)
+        if (valorAtributo >= valorMinimo && valorAtributo < valorMaximo)
+        {
+            // Se valor do atributo estiver ENTRE o valor MÍNIMO e MÁXIMO, retornar TRUE
+          Console.WriteLine("O valor do atributo está dentro do intervalo especificado"); 
+          return true;
+        }
+        else {
+            // Se valor do atributo não estiver ENTRE o valor MÍNIMO e MÁXIMO, retornar FALSE
+          Console.WriteLine("O valor do atributo está fora do intervalo especificado");
+          return false;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string atributo = Console.ReadLine();
+
+        int valorMinimo = int.Parse(Console.ReadLine());
+
+        int valorMaximo = int.Parse(Console.ReadLine());
+
+        int valorAtributo = int.Parse(Console.ReadLine());
+
+        VerificarAtributo(atributo, valorMinimo, valorMaximo, valorAtributo);
+    }
+}
